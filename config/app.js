472f10e53899738e249a32fc56a4b50b21588bc6
Tyler Kellen <tyler@sleekcode.net>
2013-09-16 15:29:29 -0400

    reduce default zoom

--- config/app.js
+++ config/app.js
@@ -3,7 +3,7 @@ module.exports = {
   description: '123',
   apikey: 'AIzaSyCuIy2sg0FV6hWvjXor7TEY1B0iJFqmKPc',
   map: {
-    zoom: 13,
+    zoom: 12,
     center: {
       lat: 45.5600,
       lng: -94.1576
